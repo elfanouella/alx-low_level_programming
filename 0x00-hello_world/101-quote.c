0db985c560ae71961ba79793c1a792e4e967df6e
nouhaila <elfa.nouhaila@gmail.com>
2023-04-23 16:05:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  *
- * 	ssize_t write(int fd, const void *buf, size_t coun);
+ *	ssize_t write(int fd, const void *buf, size_t coun);
  *
- * 	Return: 1 (error).
+ *	Return: 1 (error).
  */
 
 int main(void)
