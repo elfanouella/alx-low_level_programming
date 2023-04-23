396a6caf827335be447420460c2b5f8a6735900e
nouhaila <elfa.nouhaila@gmail.com>
2023-04-23 16:03:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
-#include<unisdt.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  *
+ * 	ssize_t write(int fd, const void *buf, size_t coun);
  *
- * Return: 1 (error).
+ * 	Return: 1 (error).
  */
-int main(void)
 
-{
-	char quo[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+int main(void)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(1, quo, 59);
 	return (1);
 }
