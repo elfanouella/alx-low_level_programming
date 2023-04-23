dc06fbbb68bf2a6b1c6d3d94f8a8b5fbdf36a055
nouhaila <elfa.nouhaila@gmail.com>
2023-04-23 13:15:56 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put fonction
+ *
+ * Return : 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+
+}
+
