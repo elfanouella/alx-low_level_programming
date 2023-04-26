e555192cd70d975d30428a57e55c76cd734a9c00
nouhaila <elfa.nouhaila@gmail.com>
2023-04-26 05:27:59 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,52 +1,35 @@
 #include "main.h"
-
-
 /**
-
  * jack_bauer - prints every minute of the day of Jack Bauer
-
  * starting from 00:00 to 23:59
-
  */
-
 void jack_bauer(void)
-
 {
+	int i, j;
 
-        int i, j;
+	i = 0;
 
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
 
-        i = 0;
+		_putchar((i % 10) + '0');
 
+		_putchar(':');
 
-        while (i < 24)
+		_putchar((j / 10) + '0');
 
-        {
+		_putchar((j % 10) + '0');
 
-                j = 0;
+		_putchar('\n');
 
-                while (j < 60)
+		j++;
+	}
 
-                {
+	i++;
 
-                        _putchar((i / 10) + '0');
-
-                        _putchar((i % 10) + '0');
-
-                        _putchar(':');
-
-                        _putchar((j / 10) + '0');
-
-                        _putchar((j % 10) + '0');
-
-                        _putchar('\n');
-
-                        j++;
-
-                }
-
-                i++;
-
-        }
-
+	}
 }
